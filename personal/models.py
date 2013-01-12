b7319d42a767a659555067a58cc42a2a2408b163
Saul Moncada <saulmoncada@gmail.com>
2013-01-12 18:18:05 -0430

    Fixes for change password

--- personal/models.py
+++ personal/models.py
@@ -179,7 +179,7 @@ if django_settings.AUTH_LDAP_BIND_PASSWORD:
         ldap_c = _LDAPConfig.get_ldap()
 
         ldap_settings = LDAPSettings()
-        conn = ldap_c.initialize(settings.AUTH_LDAP_SERVER_URI)
+        conn = ldap_c.initialize(django_settings.AUTH_LDAP_SERVER_URI)
         conn.simple_bind_s(django_settings.AUTH_LDAP_BIND_DN, django_settings.AUTH_LDAP_BIND_PASSWORD)
         for opt, value in ldap_settings.AUTH_LDAP_CONNECTION_OPTIONS.iteritems():
             conn.set_option(opt, value)
