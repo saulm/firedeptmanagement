7724a49822d7f9ad4a38ba69ad7ab3703b77a23d
Saul Moncada <saulmoncada@gmail.com>
2013-02-19 22:51:18 -0430

    Fix to remove old staff from frontpage

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,9 +1,10 @@
 from firedeptmanagement.personal.models import Firefighter
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-
+from django.conf import settings
 
 def frontpage(request):
     ff = Firefighter.objects.all()
-    data = {"ff_sample":[f for f in ff if f.profile_picture][:16]}
+    data = {}
+    data['ff_sample'] = [x for x in ff if x.current_condition_change() and x.current_condition_change().condition_id != settings.BAJ_CONDITION and x.profile_picture][:16]
     return render_to_response('frontpage.html', RequestContext(request, data))
